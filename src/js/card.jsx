b824fc635051a0d3085403f17a70ca39a6ac2641
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2018-02-13 21:07:43 +0530

    changed color of the green line and added continuous animation

--- src/js/card.jsx
+++ src/js/card.jsx
@@ -66,18 +66,32 @@ export default class toOrganCoverVizCard extends React.Component {
           <img src={data.cover_image.desktop} className="desktop-cover-image"/>
           <svg className="white-line" width="165px" height="194px" viewBox="0 0 165 194" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <defs></defs>
-            <g id="organ_cover_2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(-572.000000, -181.000000)">
-              <polyline className="white-line-path" id="Path-2" stroke="#ffffff" strokeWidth="2" points="572 368.298507 577.655172 368.298507 600.275862 373 622.896552 373 654.942529 343.850746 680.390805 328.80597 676.62069 312.820896 682.275862 310 682.275862 300.597015 694.528736 293.074627 697.356322 270.507463 703.011494 259.223881 711.494253 231.014925 721.862069 195.283582 724.689655 184 736 189.641791"></polyline>
+            <g id="organ_cover_2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(-576.000000, -185.000000)">
+              <polyline className="white-line-path" id="Path-2-Copy-2" stroke="#FFFFFF" strokeWidth="3" points="576 364 583 364 604 369 623 369 649.942529 343.567164 675.390805 328.522388 671.62069 312.537313 677.275862 309.716418 677.275862 300.313433 689.528736 292.791045 692.356322 270.223881 698.011494 258.940299 706.494253 230.731343 716.862069 195 719 187 731 187"></polyline>
             </g>
           </svg>
           <svg className="green-line" width="165px" height="194px" viewBox="0 0 165 194" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <defs></defs>
-            <g id="organ_cover_2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(-572.000000, -181.000000)">
-              <polyline className="green-line-path" id="Path-2" stroke="green" strokeWidth="3" points="572 368.298507 577.655172 368.298507 600.275862 373 622.896552 373 654.942529 343.850746 680.390805 328.80597 676.62069 312.820896 682.275862 310 682.275862 300.597015 694.528736 293.074627 697.356322 270.507463 703.011494 259.223881 711.494253 231.014925 721.862069 195.283582 724.689655 184 736 189.641791"></polyline>
+            <defs>
+              <polyline className="green-line-path" id="path-1" points="575 371.014925 580.655172 371.014925 603.275862 375.716418 625.896552 375.716418 657.942529 346.567164 683.390805 331.522388 679.62069 315.537313 685.275862 312.716418 685.275862 303.313433 697.528736 295.791045 700.356322 273.223881 706.011494 261.940299 714.494253 233.731343 724.862069 198 726 195 730 195"></polyline>
+              <filter x="-5.5%" y="-3.6%" width="111.0%" height="109.4%" filterUnits="objectBoundingBox" id="filter-2">
+                  <feMorphology radius="1.5" operator="dilate" in="SourceAlpha" result="shadowSpreadOuter1"></feMorphology>
+                  <feOffset dx="0" dy="2" in="shadowSpreadOuter1" result="shadowOffsetOuter1"></feOffset>
+                  <feMorphology radius="1.5" operator="erode" in="SourceAlpha" result="shadowInner"></feMorphology>
+                  <feOffset dx="0" dy="2" in="shadowInner" result="shadowInner"></feOffset>
+                  <feComposite in="shadowOffsetOuter1" in2="shadowInner" operator="out" result="shadowOffsetOuter1"></feComposite>
+                  <feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+                  <feColorMatrix values="0 0 0 0 0.0117647059   0 0 0 0 0.933333333   0 0 0 0 0.219607843  0 0 0 0.543195199 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
+              </filter>
+            </defs>
+            <g id="organ_cover_2" stroke="none" strokeWidth="1" fill="none" fill-rule="evenodd" transform="translate(-571.000000, -191.000000)">
+                <g id="Path-2-Copy">
+                    <use fill="black" fillOpacity="1" filter="url(#filter-2)" xlinkHref="#path-1"></use>
+                    <use stroke="#01EF36" strokeWidth="3" xlinkHref="#path-1"></use>
+                </g>
             </g>
           </svg>
           <div className="organ-selection">
-            <div className="organ-selection-button active-tab">दिल</div>
+            <div className="organ-selection-button">दिल</div>
             <div className="organ-selection-button">किडनी</div>
             <div className="organ-selection-button">लीवर</div>
             <div className="organ-selection-button mobile-more-info-button"><span className="info-icon">i</span></div>
@@ -98,17 +112,31 @@ export default class toOrganCoverVizCard extends React.Component {
       return(
         <div className="organ-cover-area-mobile">
           <div className="white-time-text"><span>{data.map_info.line_1_text}</span><br/>{data.map_info.line_1_time}</div>
-          <img src={data.cover_image.mobile} className="desktop-cover-image"/>
+          <img src={data.cover_image.mobile} className="mobile-cover-image"/>
           <svg className="white-line" width="165px" height="194px" viewBox="0 0 165 194" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <defs></defs>
-            <g id="organ_cover_2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(-572.000000, -181.000000)">
-              <polyline className="white-line-path" id="Path-2" stroke="#ffffff" strokeWidth="2" points="572 368.298507 577.655172 368.298507 600.275862 373 622.896552 373 654.942529 343.850746 680.390805 328.80597 676.62069 312.820896 682.275862 310 682.275862 300.597015 694.528736 293.074627 697.356322 270.507463 703.011494 259.223881 711.494253 231.014925 721.862069 195.283582 724.689655 184 736 189.641791"></polyline>
+            <g id="organ_cover_2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(-576.000000, -185.000000)">
+              <polyline className="white-line-path" id="Path-2-Copy-2" stroke="#FFFFFF" strokeWidth="3" points="576 364 583 364 604 369 623 369 649.942529 343.567164 675.390805 328.522388 671.62069 312.537313 677.275862 309.716418 677.275862 300.313433 689.528736 292.791045 692.356322 270.223881 698.011494 258.940299 706.494253 230.731343 716.862069 195 719 187 731 187"></polyline>
             </g>
           </svg>
           <svg className="green-line" width="165px" height="194px" viewBox="0 0 165 194" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <defs></defs>
-            <g id="organ_cover_2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(-572.000000, -181.000000)">
-              <polyline className="green-line-path" id="Path-2" stroke="green" strokeWidth="3" points="572 368.298507 577.655172 368.298507 600.275862 373 622.896552 373 654.942529 343.850746 680.390805 328.80597 676.62069 312.820896 682.275862 310 682.275862 300.597015 694.528736 293.074627 697.356322 270.507463 703.011494 259.223881 711.494253 231.014925 721.862069 195.283582 724.689655 184 736 189.641791"></polyline>
+            <defs>
+              <polyline className="green-line-path" id="path-1" points="575 371.014925 580.655172 371.014925 603.275862 375.716418 625.896552 375.716418 657.942529 346.567164 683.390805 331.522388 679.62069 315.537313 685.275862 312.716418 685.275862 303.313433 697.528736 295.791045 700.356322 273.223881 706.011494 261.940299 714.494253 233.731343 724.862069 198 726 195 730 195"></polyline>
+              <filter x="-5.5%" y="-3.6%" width="111.0%" height="109.4%" filterUnits="objectBoundingBox" id="filter-2">
+                  <feMorphology radius="1.5" operator="dilate" in="SourceAlpha" result="shadowSpreadOuter1"></feMorphology>
+                  <feOffset dx="0" dy="2" in="shadowSpreadOuter1" result="shadowOffsetOuter1"></feOffset>
+                  <feMorphology radius="1.5" operator="erode" in="SourceAlpha" result="shadowInner"></feMorphology>
+                  <feOffset dx="0" dy="2" in="shadowInner" result="shadowInner"></feOffset>
+                  <feComposite in="shadowOffsetOuter1" in2="shadowInner" operator="out" result="shadowOffsetOuter1"></feComposite>
+                  <feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+                  <feColorMatrix values="0 0 0 0 0.0117647059   0 0 0 0 0.933333333   0 0 0 0 0.219607843  0 0 0 0.543195199 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
+              </filter>
+            </defs>
+            <g id="organ_cover_2" stroke="none" strokeWidth="1" fill="none" fill-rule="evenodd" transform="translate(-571.000000, -191.000000)">
+                <g id="Path-2-Copy">
+                    <use fill="black" fillOpacity="1" filter="url(#filter-2)" xlinkHref="#path-1"></use>
+                    <use stroke="#01EF36" strokeWidth="3" xlinkHref="#path-1"></use>
+                </g>
             </g>
           </svg>
           <div className="distance-text">{data.map_info.distance}</div>
