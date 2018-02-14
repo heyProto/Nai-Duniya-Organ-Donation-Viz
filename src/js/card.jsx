70a2262b19f375dda6a9dbb4bf84782b8183f64f
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2018-02-14 19:50:34 +0530

    made transition slow, added ambulance

--- src/js/card.jsx
+++ src/js/card.jsx
@@ -52,7 +52,64 @@ export default class toOrganCoverVizCard extends React.Component {
           description: card.data.data.map_info.description_for_heart
         });
       }));
+    } else {
+      componentDidUpdate()
     }
+  }
+
+  componentDidUpdate() {
+    let path1 = document.querySelector('.white-line-path'),
+      length1 = path1.getTotalLength();
+    path1.style.transition = path1.style.WebkitTransition ='none';
+    path1.style.strokeDasharray = length1 + ' ' + length1;
+    path1.style.strokeDashoffset = length1;
+    path1.getBoundingClientRect();
+    path1.style.transition = path1.style.WebkitTransition ='stroke-dashoffset 5s linear';
+    path1.style.strokeDashoffset = '0';
+
+    let path2 = document.querySelector('.green-line-path'),
+      length2 = path2.getTotalLength();
+    path2.style.transition = path2.style.WebkitTransition ='none';
+    path2.style.strokeDasharray = length2 + ' ' + length2;
+    path2.style.strokeDashoffset = length2;
+    path2.getBoundingClientRect();
+    path2.style.transition = path2.style.WebkitTransition ='stroke-dashoffset 2.5s linear';
+    path2.style.strokeDashoffset = '0';
+
+    let circle = document.querySelector('.green-ball');
+      circle.style.transition = circle.style.WebkitTransition ='none';
+      circle.style.offsetDistance = '0%'
+      circle.getBoundingClientRect();
+      circle.style.transition = circle.style.WebkitTransition = 'offset-distance 2.5s linear';
+      circle.style.offsetDistance = '100%';
+
+    setInterval(function(){
+      let path = document.querySelector('.white-line-path'),
+        length = path.getTotalLength();
+      path.style.transition = path.style.WebkitTransition ='none';
+      path.style.strokeDasharray = length + ' ' + length;
+      path.style.strokeDashoffset = length;
+      path.getBoundingClientRect();
+      path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 5s linear';
+      path.style.strokeDashoffset = '0';
+    }, 5000)
+    setInterval(function(){
+      let path = document.querySelector('.green-line-path'),
+        length = path.getTotalLength();
+      path.style.transition = path.style.WebkitTransition ='none';
+      path.style.strokeDasharray = length + ' ' + length;
+      path.style.strokeDashoffset = length;
+      path.getBoundingClientRect();
+      path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 2.5s linear';
+      path.style.strokeDashoffset = '0';
+
+      let circle = document.querySelector('.green-ball');
+      circle.style.transition = circle.style.WebkitTransition ='none';
+      circle.style.offsetDistance = '0%'
+      circle.getBoundingClientRect();
+      circle.style.transition = circle.style.WebkitTransition = 'offset-distance 2.5s linear';
+      circle.style.offsetDistance = '100%';
+    }, 5000)
   }
 
   handleHeartClick(e){
@@ -98,18 +155,19 @@ export default class toOrganCoverVizCard extends React.Component {
       let data = this.state.dataJSON.data;
       return(
         <div className="organ-cover-area">
-          <div className="article-title">{data.map_info.headline}</div>
           <div className="white-time-text"><span>{data.map_info.line_1_text}</span><br/>{data.map_info.line_1_time}</div>
           <img src={data.cover_image.desktop} className="desktop-cover-image"/>
           <svg className="white-line" width="165px" height="194px" viewBox="0 0 165 194" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <defs></defs>
             <g id="organ_cover_2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(-576.000000, -185.000000)">
-              <polyline className="white-line-path" id="Path-2-Copy-2" stroke="#FFFFFF" strokeWidth="3" points="576 364 583 364 604 369 623 369 649.942529 343.567164 675.390805 328.522388 671.62069 312.537313 677.275862 309.716418 677.275862 300.313433 689.528736 292.791045 692.356322 270.223881 698.011494 258.940299 706.494253 230.731343 716.862069 195 719 187 731 187"></polyline>
+              <path className="white-line-path" id="path-2" stroke="#FFFFFF" strokeWidth="3" d="M576 364 583 364 604 369 623 369 649.942529 343.567164 675.390805 328.522388 671.62069 312.537313 677.275862 309.716418 677.275862 300.313433 689.528736 292.791045 692.356322 270.223881 698.011494 258.940299 706.494253 230.731343 716.862069 195 719 187 731 187" strokeDasharray="283.0320129394531, 283.0320129394531" strokeDashoffset="283.0320129394531">
+              </path>
             </g>
           </svg>
+          <img className="white-ball" src="amb-icon.png"/>
           <svg className="green-line" width="165px" height="194px" viewBox="0 0 165 194" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <defs>
-              <polyline className="green-line-path" id="path-1" points="575 371.014925 580.655172 371.014925 603.275862 375.716418 625.896552 375.716418 657.942529 346.567164 683.390805 331.522388 679.62069 315.537313 685.275862 312.716418 685.275862 303.313433 697.528736 295.791045 700.356322 273.223881 706.011494 261.940299 714.494253 233.731343 724.862069 198 726 195 730 195"></polyline>
+              <path className="green-line-path" id="path-1" d="M575 371.014925 580.655172 371.014925 603.275862 375.716418 625.896552 375.716418 657.942529 346.567164 683.390805 331.522388 679.62069 315.537313 685.275862 312.716418 685.275862 303.313433 697.528736 295.791045 700.356322 273.223881 706.011494 261.940299 714.494253 233.731343 724.862069 198 726 195 730 195"></path>
               <filter x="-5.5%" y="-3.6%" width="111.0%" height="109.4%" filterUnits="objectBoundingBox" id="filter-2">
                   <feMorphology radius="1.5" operator="dilate" in="SourceAlpha" result="shadowSpreadOuter1"></feMorphology>
                   <feOffset dx="0" dy="2" in="shadowSpreadOuter1" result="shadowOffsetOuter1"></feOffset>
@@ -121,12 +179,13 @@ export default class toOrganCoverVizCard extends React.Component {
               </filter>
             </defs>
             <g id="organ_cover_2" stroke="none" strokeWidth="1" fill="none" fill-rule="evenodd" transform="translate(-571.000000, -191.000000)">
-                <g id="Path-2-Copy">
+                <g id="Path-2">
                     <use fill="black" fillOpacity="1" filter="url(#filter-2)" xlinkHref="#path-1"></use>
                     <use stroke="#01EF36" strokeWidth="3" xlinkHref="#path-1"></use>
                 </g>
             </g>
           </svg>
+          <img className="green-ball" src="amb-icon.png"/>
           <div className="organ-selection">
             <div id="heart" className="organ-selection-button active-tab" onClick={(e)=>{ this.handleHeartClick(e) }}>दिल</div>
             <div id="kidney" className="organ-selection-button" onClick={(e)=>{ this.handleKidneyClick(e) }}>किडनी</div>
@@ -134,7 +193,7 @@ export default class toOrganCoverVizCard extends React.Component {
           </div>
           <div className="distance-text">{data.map_info.distance}</div>
           <div className="time-text"><span>{data.map_info.line_2_text}</span><br/>{data.map_info.line_2_time}</div>
-          <div className="intro-text">{this.state.description}</div>
+          <div className="intro-text">{data.map_info.description}</div>
         </div>
       )
     }
@@ -212,3 +271,12 @@ export default class toOrganCoverVizCard extends React.Component {
     return text_obj;
   }
 }
+
+// <circle id="white-circle" r="5" cx="0" cy="0" fill="white">  
+//                 <animateMotion 
+//                   xlinkHref="#white-circle"
+//                   dur="1.4s"
+//                   repeatCount="indefinite">
+//                   <mpath xlinkHref="#path-2" />
+//                 </animateMotion>
+//               </circle>
